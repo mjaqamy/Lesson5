d1f3baa7884debadbc9963e116f75e6528e54de9
mjaqamy <mjaqamy@gmail.com>
2026-01-28 21:56:26 +0300

    feat: resolve task4

--- task4.ts
+++ task4.ts
@@ -2,3 +2,20 @@
 // Пароль должен содержать как минимум одну заглавную букву и цифру
 // Пароль только из латинских букв
 // Длина пароля должна быть не менее 10 символов
+
+function checkPassword(password: string) {
+  if (password.length < 10) {
+    return false;
+  }
+  if (!/[A-Z]/g.test(password)) {
+    return false;
+  }
+  if (!/[0-9]/g.test(password)) {
+    return false;
+  }
+  if (/[А-Яа-яЁё]/g.test(password)) {
+    return false;
+  }
+  return true;
+}
+console.log(checkPassword("sdfghjjhh0Dh"));
